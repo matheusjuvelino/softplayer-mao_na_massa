fa6c7c9a3df70a41fb7dfc55978d0292c80cbe29
matheusjuvelinop21 <matheus.silva@p21sistemas.com.br>
2021-04-10 16:11:05 -0300

    implementou functional tests api calcular juros

---
+++ src/Services/Juros/CalcularJuros/CalcularJuros.FunctionalTests/CalcularJurosScenarios.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CalcularJuros.FunctionalTests
+{
+    public class CalcularJurosScenarios : CalcularJurosScenariosBase
+    {
+
+        [Fact]
+        public async Task Obter_taxa_de_juros_response_ok_status_code()
+        {
+            using var server = CreateServer();
+            var response = await server.CreateClient()
+                .GetAsync(Get.CalculaJuros("100", "5"));
+
+            response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async Task Obter_taxa_de_juros_response_bad_request_status_code()
+        {
+            using var server = CreateServer();
+            var response = await server.CreateClient()
+                .GetAsync(Get.CalculaJuros(null, null));
+
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+    }
+}
