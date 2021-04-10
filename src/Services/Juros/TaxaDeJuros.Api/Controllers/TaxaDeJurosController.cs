90cb8c8e84b41bf47fdfd1d3ea8b78bd3cceaac1
matheusjuvelinop21 <matheus.silva@p21sistemas.com.br>
2021-04-10 10:35:12 -0300

    implementou funcionalidade obter taxa de juros

--- src/Services/Juros/TaxaDeJuros.Api/Controllers/TaxaDeJurosController.cs
+++ src/Services/Juros/TaxaDeJuros.Api/Controllers/TaxaDeJurosController.cs
@@ -1,17 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace TaxaDeJuros.Api.Controllers
 {
-    [Route("api/v1/[controller]")]
+    [Route("api/v1/taxaJuros")]
     [ApiController]
     public class TaxaDeJurosController : ControllerBase
     {
+        private readonly TaxaDeJurosSettings _settings;
         private readonly ILogger<TaxaDeJurosController> _logger;
 
-        public TaxaDeJurosController(ILogger<TaxaDeJurosController> logger)
+        public TaxaDeJurosController(IOptionsSnapshot<TaxaDeJurosSettings> settings, ILogger<TaxaDeJurosController> logger)
         {
+            _settings = settings.Value;
             _logger = logger;
+        }
+
+        // GET api/v1/taxaJuros
+        [HttpGet]
+        [Route("")]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public ActionResult<string> Get()
+        {
+            if (string.IsNullOrWhiteSpace(_settings.TaxaBaseDeJuros))
+            {
+                return BadRequest("Não foi informado o valor base para taxa de juros.");
+            }
+
+            return _settings.TaxaBaseDeJuros;
         }
     }
 }
--- src/Services/Juros/TaxaDeJuros.UnitTests/TaxaDeJurosControllerTest.cs
+++ src/Services/Juros/TaxaDeJuros.UnitTests/TaxaDeJurosControllerTest.cs
@@ -1,9 +1,58 @@
-using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using TaxaDeJuros.Api;
+using TaxaDeJuros.Api.Controllers;
 using Xunit;
 
 namespace TaxaDeJuros.UnitTests
 {
     public class TaxaDeJurosControllerTest
     {
+        private readonly Mock<ILogger<TaxaDeJurosController>> _loggerMock;
+
+        public TaxaDeJurosControllerTest()
+        {
+            _loggerMock = new Mock<ILogger<TaxaDeJurosController>>();
+        }
+
+        [Fact]
+        public void Obter_taxa_de_juros_com_sucesso()
+        {
+            var taxaDeJurosExperada = "0,01";
+            var taxaDeJurosSettingsMock = ConfigurarTaxaDeJurosSettingsMock(taxaDeJurosExperada);
+
+            var taxaDeJurosController = new TaxaDeJurosController(taxaDeJurosSettingsMock.Object, _loggerMock.Object);
+
+            var actionResult = taxaDeJurosController.Get();
+
+
+            Assert.IsType<ActionResult<string>>(actionResult);
+            var taxaBaseDeJuros = Assert.IsAssignableFrom<string>(actionResult.Value);
+            Assert.Equal(taxaDeJurosExperada, taxaBaseDeJuros);
+        }
+
+        [Fact]
+        public void Obter_taxa_de_juros_com_erro()
+        {
+            var taxaDeJurosSettingsMock = ConfigurarTaxaDeJurosSettingsMock(null);
+
+            var taxaDeJurosController = new TaxaDeJurosController(taxaDeJurosSettingsMock.Object, _loggerMock.Object);
+
+            var actionResult = taxaDeJurosController.Get();
+
+            Assert.IsType<BadRequestObjectResult>(actionResult.Result);
+            var badRequestObjectResult = Assert.IsAssignableFrom<BadRequestObjectResult>(actionResult.Result);
+            Assert.NotNull(badRequestObjectResult.Value);
+        }
+
+        private static Mock<IOptionsSnapshot<TaxaDeJurosSettings>> ConfigurarTaxaDeJurosSettingsMock(string value)
+        {
+            var taxaDeJurosSettingsMock = new Mock<IOptionsSnapshot<TaxaDeJurosSettings>>();
+            taxaDeJurosSettingsMock.Setup(t => t.Value).Returns(new TaxaDeJurosSettings { TaxaBaseDeJuros = value });
+
+            return taxaDeJurosSettingsMock;
+        }
     }
 }
