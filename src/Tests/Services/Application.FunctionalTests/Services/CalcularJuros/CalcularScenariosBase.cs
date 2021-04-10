6d406b23e4f65170c8f5e419f3daeb10cc23a7e6
matheusjuvelinop21 <matheus.silva@p21sistemas.com.br>
2021-04-10 19:38:55 -0300

    implementou testes funcionais integrando as applicações

---
+++ src/Tests/Services/Application.FunctionalTests/Services/CalcularJuros/CalcularScenariosBase.cs
@@ -0,0 +1,41 @@
+﻿
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Reflection;
+
+namespace FunctionalTests.Services.CalcularJuros
+{
+    public class CalcularJurosScenariosBase
+    {
+        private const string ApiUrlBase = "api/v1";
+
+
+        public static TestServer CreateServer()
+        {
+            var path = Assembly.GetAssembly(typeof(CalcularJurosScenariosBase))
+               .Location;
+
+            var hostBuilder = new WebHostBuilder()
+                .UseContentRoot(Path.GetDirectoryName(path))
+                .ConfigureAppConfiguration(cb =>
+                {
+                    cb.AddJsonFile("Services/CalcularJuros/appsettings.json", optional: false)
+                    .AddEnvironmentVariables();
+                })
+                .UseStartup<CalcularJurosTestsStartup>()
+                .UseUrls("http://localhost:5005");
+
+            return new TestServer(hostBuilder);
+        }
+
+        public static class Get
+        {
+            public static string CalculaJuros(string valorInicial, string meses)
+            {
+                return $"{ApiUrlBase}/calculajuros?valorinicial={valorInicial}&meses={meses}";
+            }
+        }
+    }
+}
---
+++ src/Tests/Services/Application.FunctionalTests/Services/CalcularJuros/CalcularTestsStartup.cs
@@ -0,0 +1,41 @@
+﻿
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using CalcularJuros.Api;
+using FunctionalTests.Services.TaxaDeJuros;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+
+namespace FunctionalTests.Services.CalcularJuros
+{
+    class CalcularJurosTestsStartup : Startup
+    {
+        public CalcularJurosTestsStartup(IConfiguration env) : base(env)
+        {
+        }
+
+        public override IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            base.ConfigureServices(services);
+
+
+            services.AddSingleton(typeof(IHttpClientFactory), new TaxaDeJurosHttpClientFactory());
+
+            var container = new ContainerBuilder();
+            container.Populate(services);
+
+            return new AutofacServiceProvider(container.Build());
+        }
+    }
+
+    class TaxaDeJurosHttpClientFactory : IHttpClientFactory
+    {
+        public HttpClient CreateClient(string name)
+        {
+            var taxaDeJurosServer = TaxaDeJurosScenariosBase.CreateServer();
+            return taxaDeJurosServer.CreateClient();
+        }
+    }
+}
