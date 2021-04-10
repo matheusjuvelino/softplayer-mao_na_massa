3c969b7993dbca39b7957d8e39a79fb88567d693
matheusjuvelino <matheus.juvelino@outlook.com>
2021-04-10 20:56:37 -0300

    implementou busca taxa de juros via GRPC

---
+++ src/Services/Juros/CalcularJuros/CalcularJuros.Api/Services/TaxaDeJurosServiceGrpc.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Globalization;
+using GrpcTaxaDeJuros;
+using System.Threading.Tasks;
+
+namespace CalcularJuros.Api.Services
+{
+    public class TaxaDeJurosServiceGrpc : ITaxaDeJurosService
+    {
+        private readonly TaxaDeJuros.TaxaDeJurosClient _taxaDeJurosClient;
+        private readonly string _remoteServiceBaseUrl;
+
+        public TaxaDeJurosServiceGrpc(TaxaDeJuros.TaxaDeJurosClient taxaDeJurosClient, IOptionsSnapshot<CalcularJurosSettings> settings)
+        {
+            _taxaDeJurosClient = taxaDeJurosClient ?? throw new ArgumentNullException(nameof(taxaDeJurosClient));
+
+            _remoteServiceBaseUrl = $"{settings.Value.TaxaDeJurosUrl}/taxaJuros";
+        }
+
+        public async Task<decimal> ObterTaxaDeJuros()
+        {
+            var responseTaxaDeJuros = await _taxaDeJurosClient.ObterAsync(new ObterTaxaDeJuros());
+
+            return decimal.Parse(responseTaxaDeJuros.Valor, CultureInfo.GetCultureInfo("pt-BR"));
+        }
+    }
+}
--- src/Services/Juros/TaxaDeJuros/TaxaDeJuros.Api/Grpc/TaxaDeJurosService.cs
+++ src/Services/Juros/TaxaDeJuros/TaxaDeJuros.Api/Grpc/TaxaDeJurosService.cs
@@ -1,14 +1,34 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Grpc.Core;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Threading.Tasks;
+using TaxaDeJuros.Api;
 
 namespace GrpcTaxaDeJuros
 {
     public class TaxaDeJurosService : TaxaDeJuros.TaxaDeJurosBase
     {
+        private readonly TaxaDeJurosSettings _settings;
         private readonly ILogger<TaxaDeJurosService> _logger;
 
-        public TaxaDeJurosService(ILogger<TaxaDeJurosService> logger)
+        public TaxaDeJurosService(IOptionsSnapshot<TaxaDeJurosSettings> settings, ILogger<TaxaDeJurosService> logger)
         {
+            _settings = settings.Value;
             _logger = logger;
+        }
+
+        [AllowAnonymous]
+        public override Task<TaxaDeJurosResponse> Obter(ObterTaxaDeJuros request, ServerCallContext context)
+        {
+            if (string.IsNullOrWhiteSpace(_settings.TaxaBaseDeJuros))
+            {
+                context.Status = new Status(StatusCode.NotFound, "Não foi informado o valor base para taxa de juros.");
+                return null;
+            }
+
+            context.Status = new Status(StatusCode.OK, "Taxa base de juros obtida com sucesso");
+            return Task.FromResult(new TaxaDeJurosResponse { Valor = _settings.TaxaBaseDeJuros });
         }
     }
 }
