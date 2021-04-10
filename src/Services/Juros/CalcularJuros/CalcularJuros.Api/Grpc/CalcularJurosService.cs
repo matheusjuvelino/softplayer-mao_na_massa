044042dbbf2a943951905a708dd342735405a96a
matheusjuvelinop21 <matheus.silva@p21sistemas.com.br>
2021-04-10 11:12:59 -0300

    adicionou base CalcularJuros.Api

---
+++ src/Services/Juros/CalcularJuros/CalcularJuros.Api/Grpc/CalcularJurosService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace GrpcCalcularJuros
+{
+    public class CalcularJurosService : CalcularJuros.CalcularJurosBase
+    {
+        private readonly ILogger<CalcularJurosService> _logger;
+
+        public CalcularJurosService(ILogger<CalcularJurosService> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
---
+++ src/Services/Juros/CalcularJuros/CalcularJuros.Api/Infrastructure/Exceptions/CalcularJurosDomainException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CalcularJuros.Api.Infrastructure.Exceptions
+{
+    public class CalcularJurosDomainException : Exception
+    {
+        public CalcularJurosDomainException()
+        { }
+
+        public CalcularJurosDomainException(string message)
+            : base(message)
+        { }
+
+        public CalcularJurosDomainException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}
