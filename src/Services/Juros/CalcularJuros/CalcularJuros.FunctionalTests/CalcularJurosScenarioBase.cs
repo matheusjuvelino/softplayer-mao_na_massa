9e139a71af21930da40486875b3740aada5cef0b
matheusjuvelinop21 <matheus.silva@p21sistemas.com.br>
2021-04-10 16:11:05 -0300

    implementou functional tests api calcular juros

---
+++ src/Services/Juros/CalcularJuros/CalcularJuros.FunctionalTests/CalcularJurosScenarioBase.cs
@@ -0,0 +1,42 @@
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System.IO;
+using System.Reflection;
+using CalcularJuros.Api;
+
+namespace CalcularJuros.FunctionalTests
+{
+    public class CalcularJurosScenariosBase
+    {
+        public TestServer CreateServer()
+        {
+            var path = Assembly.GetAssembly(typeof(CalcularJurosScenariosBase))
+              .Location;
+
+            var hostBuilder = new WebHostBuilder()
+                .UseContentRoot(Path.GetDirectoryName(path))
+                .ConfigureAppConfiguration(cb =>
+                {
+                    cb.AddJsonFile("appsettings.json", optional: false)
+                    .AddEnvironmentVariables();
+                })
+                .UseStartup<Startup>();
+
+
+            var testServer = new TestServer(hostBuilder);
+
+            return testServer;
+        }
+
+        public static class Get
+        {
+            public static string CalculaJuros(string valorInicial, string meses)
+            {
+                return $"api/v1/calculajuros?valorinicial={valorInicial}&meses={meses}";
+            }
+        }
+    }
+}
