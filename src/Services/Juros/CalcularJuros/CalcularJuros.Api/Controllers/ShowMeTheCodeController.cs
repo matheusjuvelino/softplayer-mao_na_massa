073906c190d63a1013c8573ffbfa85d72ffe44eb
matheusjuvelino <matheus.juvelino@outlook.com>
2021-04-10 21:16:23 -0300

    implementou funcionalidade showmethecode

---
+++ src/Services/Juros/CalcularJuros/CalcularJuros.Api/Controllers/ShowMeTheCodeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace CalcularJuros.Api.Controllers
+{
+    [Route("api/v1/showmethecode")]
+    public class ShowMeTheCodeController
+    {
+        [HttpGet]
+        [Route("")]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.OK)]
+        public ActionResult<string> Get()
+        {
+            return "https://github.com/matheusjuvelino/softplayer-mao_na_massa";
+        }
+    }
+}
